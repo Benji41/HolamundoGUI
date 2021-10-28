2285773e10aeb20b3c08d83c98179a47aa1d6d06
Benji41 <noereynoso.business@gmail.com>
2021-10-28 16:35:40 -0700

    first commit

---
+++ HolaMundoGUI/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HolaMundoGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.label1.Text = Main().ToString();
+        }
+
+        public static int Main() { 
+            int numero = 2;
+            int numero2 = numero / j;
+            if(numero = 2)
+            {
+                Console.WriteLine("EXITO");
+            }
+            return 0;
+        }
+
+    }
+}
